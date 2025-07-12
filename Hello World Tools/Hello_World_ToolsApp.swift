79559b38b8311e2e1b9661a5264dd8748a5ac276
MAOShea <michael.a.oshea@gmail.com>
2025-07-12 19:00:39 +0200

    Initial Commit

---
+++ Hello World Tools/Hello_World_ToolsApp.swift
@@ -0,0 +1,17 @@
+//
+//  Hello_World_ToolsApp.swift
+//  Hello World Tools
+//
+//  Created by mike on 12/07/2025.
+//
+
+import SwiftUI
+
+@main
+struct Hello_World_ToolsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Hello World ToolsTests/Hello_World_ToolsTests.swift
@@ -0,0 +1,17 @@
+//
+//  Hello_World_ToolsTests.swift
+//  Hello World ToolsTests
+//
+//  Created by mike on 12/07/2025.
+//
+
+import Testing
+@testable import Hello_World_Tools
+
+struct Hello_World_ToolsTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
